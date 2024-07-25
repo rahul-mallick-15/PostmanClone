2f896b107237d84804f9a724e3210a16954399d3
Rahul Mallick <rahulmallick1115@gmail.com>
2024-07-26 00:33:51 +0530

    Use compression-webpack-plugin for gzip

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const CompressionWebpackPlugin = require("compression-webpack-plugin");
 
 module.exports = {
   mode: "production",
@@ -22,4 +23,13 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new CompressionWebpackPlugin({
+      filename: "bundle.js.gz", // Output file name
+      algorithm: "gzip", // Compression algorithm
+      test: /\.(js|css|html|svg)$/, // File types to compress
+      threshold: 10240, // Only assets bigger than this size are processed
+      minRatio: 0.8, // Only assets that compress better than this ratio are processed
+    }),
+  ],
 };
